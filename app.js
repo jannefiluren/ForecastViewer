7b929bec010a67fad75677febcd5c3b00415b026
jmg <jmg@nve.no>
2018-08-14 15:43:56 +0200

    switch to materialize

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-
-
 function createDivs(data) {
-    
   const allPlots = document.querySelector("#allplots");
-  
+
   let html = [];
   for (tmp in data) {
     html += `
@@ -11,20 +8,16 @@ function createDivs(data) {
     <div id="${tmp}" class="singlePlot"></div>
     </div>
     `;
-    
   }
   allPlots.innerHTML = html;
-  
+
   for (tmp in data) {
-    plot(tmp, data[tmp])
+    plot(tmp, data[tmp]);
   }
-    
 }
 
-
 function plot(catchmentName, data) {
-
-  for (let i=0; i<data.qpast.length; i++) {
+  for (let i = 0; i < data.qpast.length; i++) {
     if (data.qpast[i] === -9999) {
       data.qpast[i] = NaN;
     }
@@ -36,42 +29,41 @@ function plot(catchmentName, data) {
   let traceTair = {
     x: data.time,
     y: data.tair,
-    name: 'Air temperature',
-    type: 'scatter'
-  }
-  
+    name: "Air temperature",
+    type: "scatter"
+  };
+
   let tracePrec = {
     x: data.time,
     y: data.prec,
-    yaxis: 'y2',
-    name: 'Precipitation',
-    type: 'bar',
+    yaxis: "y2",
+    name: "Precipitation",
+    type: "bar",
     marker: {
-      color: 'blue',
+      color: "blue"
     },
     opacity: 0.5
-  }
-  
+  };
+
   let traceQobs = {
     x: data.time,
     y: data.qpast,
-    yaxis: 'y3',
-    name: 'Observed runoff',
-    type: 'scatter'
+    yaxis: "y3",
+    name: "Observed runoff",
+    type: "scatter"
   };
-  
+
   let traceQsim = {
     x: data.time,
     y: data.qfuture,
-    yaxis: 'y3',
-    name: 'Forecasted runoff',
-    type: 'scatter'
+    yaxis: "y3",
+    name: "Forecasted runoff",
+    type: "scatter"
   };
 
   let layout = {
-
     height: "600",
-    
+
     xaxis: {
       showgrid: true
     },
@@ -81,68 +73,58 @@ function plot(catchmentName, data) {
       domain: [0, 0.45],
       showgrid: false
     },
-    
+
     yaxis2: {
-      title: 'Precipitation (mm/hr)',
-      overlaying: 'y',
-      side: 'right',
-      autorange: 'reversed'
+      title: "Precipitation (mm/hr)",
+      overlaying: "y",
+      side: "right",
+      autorange: "reversed"
     },
-    
+
     yaxis3: {
       domain: [0.5, 1.0],
       title: "Runoff (m<sup>3</sup>/s)"
     },
-    
+
     title: catchmentName,
-    
+
     titlefont: {
       size: 30
     },
 
-    paper_bgcolor: '#e5e5e5'
-
-  }
+    paper_bgcolor: "#e5e5e5"
+  };
 
   let traces = [traceQobs, traceQsim, traceTair, tracePrec];
-  
+
   console.time();
 
-  Plotly.newPlot(catchmentName, traces, layout, {displayModeBar: false});
-  
+  Plotly.newPlot(catchmentName, traces, layout, { displayModeBar: false });
+
   console.timeEnd();
-
 }
-
 
 fetch("data/data.json", {
   header: {
     "Content-type": "application/json"
   }
-}).then(res => res.json())
+})
+  .then(res => res.json())
   .then(data => createDivs(data))
   .catch(err => console.log(err));
-
-
 
 const selectStation = document.getElementById("selectStation");
 
 selectStation.addEventListener("keyup", () => {
-
   let plots = document.querySelectorAll(".singlePlot");
 
   let searchString = document.getElementById("selectStation").value;
 
-  plots.forEach((plot) => {
-
+  plots.forEach(plot => {
     if (plot.getAttribute("id").indexOf(searchString) !== -1) {
       plot.parentElement.style.display = "block";
     } else {
       plot.parentElement.style.display = "none";
     }
-
-  })
-
-})
-
-
+  });
+});
